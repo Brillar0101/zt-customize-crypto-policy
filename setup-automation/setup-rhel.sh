4b01ba49e4387c01cd05a542a3f4ccc99f7256ae
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 12:04:57 -0400

    Fix setup-rhel.sh comment placement

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Install the mod_ssl moved to instance.yml
-# dnf install -y httpd mod_ssl
+
 # Unregister and register the VM
 subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
 
+# Install the mod_ssl moved to instance.yml
+# dnf install -y httpd mod_ssl
 systemctl --now enable httpd
 
 # Once we have a SSL cert created at DEFAULT 2048 bit, we can up the policy for the lab
