100e6b58bf2751e5294d66650ce0d1dac74f1f83
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 10:23:22 -0400

    Unregister and re-register the system

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Install the mod_ssl moved to instance.yml
 # dnf install -y httpd mod_ssl
+# Unregister and register the VM
+subscription-manager clean
+subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+
 systemctl --now enable httpd
 
 # Once we have a SSL cert created at DEFAULT 2048 bit, we can up the policy for the lab
