dd2ff5dc2b8e20bd1bdf160449bef60976dd893e
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-07-14 10:14:43 -0400

    fix ssh issue

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -12,3 +12,7 @@ systemctl --now enable httpd
 # Once we have a SSL cert created at DEFAULT 2048 bit, we can up the policy for the lab
 update-crypto-policies --set FUTURE
 
+# fix ssh issue
+echo "KexAlgorithms +curve25519-sha256,ecdh-sha2-nistp256,ecdh-sha2-nistp384" >> /etc/ssh/sshd_config
+systemctl restart sshd
+
